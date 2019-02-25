8139174c8df0535d237e13156acc3e09facd50e2
Thulio Carvalho <thulioicc@gmail.com>
2019-02-25 18:40:09 -0300

    added naive structural code

---
+++ OCRErrorReduction.py
@@ -0,0 +1,13 @@
+class OCRErrorReduction:
+
+    keyphrases = []
+
+    def initialize(self, keyphrases):
+        self.keyphrases = keyphrases
+
+    def clean(self, paragraph):
+        # Returns paragraph only for testing purporses
+        return paragraph
+
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+    pass
+
+# Should compute the maximum number of words that 
+# can be matched in the string to the dictionary.
+#
+# Maybe Aho-Corasick is a good approach (?)
+def match(CS, D):
+    pass
+
+# Should compute the edit distance between the strings
+#
+# Maybe Needleman-Wunsch algorithm (?)
+def editDistance(CS, OS):
+    pass
+    
+# Raw score of error reduction algorigthm
+def rawScore(CS, OD, D):
+    pass
+
+if __name__ == "__main__":
+    main()
